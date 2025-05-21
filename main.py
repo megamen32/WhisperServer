4108c7370abda24bc374ff9edb5d427976a4eb6e
megam <megamen932@gmail.com>
2025-05-21 14:54:37 +0300

    bug fix

--- main.py
+++ main.py
@@ -221,4 +221,4 @@ async def transcribe(
 if __name__ == "__main__":
     import uvicorn
     import torch
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7653)
